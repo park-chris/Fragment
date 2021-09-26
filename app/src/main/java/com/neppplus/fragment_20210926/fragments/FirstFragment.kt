27b0d1964db2ef0488ffcecc624d6ac2347e544f
박수정 <zxzx4342@naver.com>
2021-09-26 12:04:16 +0900

    프래그먼트의 동작 코드 - onActivityCreated 활용

--- app/src/main/java/com/neppplus/fragment_20210926/fragments/FirstFragment.kt
+++ app/src/main/java/com/neppplus/fragment_20210926/fragments/FirstFragment.kt
@@ -1,11 +1,13 @@
 package com.neppplus.fragment_20210926.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.neppplus.fragment_20210926.R
+import kotlinx.android.synthetic.main.fragment_first.*
 
 class FirstFragment : Fragment() {
 
@@ -15,6 +17,15 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        logBtn.setOnClickListener {
+            Log.d("첫 부품", " 버튼 눌림림")
+       }
+
     }
 
 }
