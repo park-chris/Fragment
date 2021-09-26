070c55baa854144b15c440e27ed790b694a8f07c
박수정 <zxzx4342@naver.com>
2021-09-26 12:29:31 +0900

    두번째 프래그먼트에서 requireContext() 활용

--- app/src/main/java/com/neppplus/fragment_20210926/fragments/SecondFragment.kt
+++ app/src/main/java/com/neppplus/fragment_20210926/fragments/SecondFragment.kt
@@ -23,7 +23,13 @@ class SecondFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
 
         secondLogBtn.setOnClickListener {
-            Toast.makeText(activity, "두번째 프래그먼트", Toast.LENGTH_SHORT).show()
+
+//            코드 적는 곳 (this) : 부품
+//            토스트의 재료로 필요한것 ( ) : 화면 불러내려면?
+
+//            Toast.makeText(activity, "두번째 프래그먼트", Toast.LENGTH_SHORT).show()
+            Toast.makeText(requireContext(), "두번째 프래그먼트", Toast.LENGTH_SHORT).show()
+
         }
     }
 
